07278b7fc4d38dc75f99777a5cd311eda9b8e0cd
unknown <brucolon@gmail.com>
2026-01-05 18:05:32 -0300

    Navbar Style

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import styles from "./Navbar.module.scss";
+import { FaSpotify } from "react-icons/fa";
+import { GoHome, GoSearch, GoFileDirectory, GoDownload } from "react-icons/go";
+
+export default function Navbar() {
+  return (
+    <nav className={styles.nav}>
+      <Link href="/" className={styles.logo}>
+        <FaSpotify size={34} />
+      </Link>
+      <div className={styles.navItems}>
+        <Link href="/" className={styles.homeIcon}>
+          <GoHome size={25} />
+        </Link>
+        <div className={styles.searchContainer}>
+          <div className={styles.searchIcon}>
+            <GoSearch size={25} />
+          </div>
+          <input type="text" placeholder="¿Qué quieres reproducir?" />
+          <div className={styles.searchIcon}>
+            <GoFileDirectory size={25} />
+          </div>
+        </div>
+      </div>
+      <div className={styles.navActions}>
+        <div className={styles.installApp}>
+          <GoDownload size={20} />
+          <span>Instalar App</span>
+        </div>
+        <div className={styles.profile}>
+          <div className={styles.avatar}>
+            <span>B</span>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
